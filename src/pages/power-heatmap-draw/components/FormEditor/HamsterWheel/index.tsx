8b05f54e9b65f1aa63658a70dd3a2c0f24473195
shenguohao <shenguohao@bytedance.com>
2023-05-31 15:37:53 +0800

    feat: css opt

---
+++ src/pages/power-heatmap-draw/components/FormEditor/HamsterWheel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function LoaderWrapper() {
+  return (
+    <div className='css-container'>
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default LoaderWrapper;
